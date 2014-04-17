8a37445685ec1b5ba9810bf4405ed769b7a8a85c
Sangmin Yoon <sangmin.yoon@croquis.com>
2014-04-17 14:56:14 +0900

    cake stop 추가

--- cakefile.js
+++ cakefile.js
@@ -13,6 +13,12 @@ task('start', 'Starts this app as daemon', function() {
   spawn(command, args, { stdio: 'inherit' });
 });
 
+task('stop', 'Stops the app daemon', function() {
+  var command = __dirname + '/bin/stop';
+  var args = [];
+  spawn(command, args, { stdio: 'inherit' });
+});
+
 task('run', 'Runs the server', function () {
   var command = path.resolve(require.resolve('coffee-script/register'), '../bin/coffee');
   var args = [__dirname + '/lib/server.coffee', '-w', '-d'];
