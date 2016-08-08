cfc1a9a9c59482d30a0071d90f29ae940fcc162e
Sangmin Yoon <sangmin.yoon@croquis.com>
2016-08-08 16:07:29 +0900

    v0.7.5

--- bin/_deploy_on_server.sh
+++ bin/_deploy_on_server.sh
@@ -27,7 +27,7 @@ cat <<EOF > package.json
 {
   "dependencies": {
     "coffee-script": "^1.10.0",
-    "@croquiscom/croquis.deployer": "^0.7.4",
+    "@croquiscom/croquis.deployer": "^0.7.5",
     "forever": "^0.15.2",
     "js-yaml": "^3.6.1"
   }
