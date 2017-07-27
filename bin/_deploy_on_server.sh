4452360181ad62711c5b52e4cdcd510266e8794c
Sangmin Yoon <sangmin.yoon@croquis.com>
2017-07-27 12:19:14 +0900

    출력 개선

--- bin/_deploy_on_server.sh
+++ bin/_deploy_on_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+COLOR_BLUE='\033[0;34m'
+COLOR_MAGENTA='\033[0;35m'
+COLOR_RESET='\033[0m'
+
 PROJECT_NAME=$1
 REF=$2
 ROOT=~/running/$PROJECT_NAME
@@ -12,16 +16,16 @@ cd `dirname "$SELF"`
 NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-echo !- Installing project node modules
+echo -e ${COLOR_BLUE}!- Installing project node modules${COLOR_RESET}
 npm install --production
 
-echo !- Clean copy of repositories to $ROOT/$TARGET
+echo -e ${COLOR_BLUE}!- Clean copy of repositories to ${COLOR_MAGENTA}$ROOT/$TARGET${COLOR_RESET}
 mkdir -p $ROOT/$TARGET
 # croquis.deployer는 deployer 모듈로 설치되므로 app 모듈에서는 제외한다.
 # (두개가 있으면 forever가 server 프로세스를 다른 스크립트로 인식할 가능성이 있다)
 rsync --exclude=croquis.deployer -az . $ROOT/$TARGET
 
-echo !- Installing deployer node modules
+echo -e ${COLOR_BLUE}!- Installing deployer node modules${COLOR_RESET}
 cd $ROOT
 cat <<EOF > package.json
 {
@@ -33,13 +37,13 @@ cat <<EOF > package.json
   }
 }
 EOF
-npm install
+npm --loglevel=error install
 
-echo !- Make live link
+echo -e ${COLOR_BLUE}!- Make live link${COLOR_RESET}
 rm -f $CURRENT
 ln -s $TARGET $CURRENT
 
-echo !- Install logrotate
+echo -e ${COLOR_BLUE}!- Install logrotate${COLOR_RESET}
 cat <<EOF > $CURRENT/logrotate.conf
 $HOME/.croquis/$PROJECT_NAME.log {
     daily
@@ -55,9 +59,9 @@ $HOME/.croquis/$PROJECT_NAME.log {
 }
 EOF
 
-echo !- Run server
+echo -e ${COLOR_BLUE}!- Run server${COLOR_RESET}
 export PROJECT_ROOT=$ROOT/$CURRENT
 ./node_modules/@croquiscom/croquis.deployer/bin/start || echo '(((***** FAIL TO START *****)))'
 
-echo !- Install Cron jobs
+echo -e ${COLOR_BLUE}!- Install Cron jobs${COLOR_RESET}
 ./node_modules/.bin/coffee ./node_modules/@croquiscom/croquis.deployer/bin/install_cron_jobs.coffee
